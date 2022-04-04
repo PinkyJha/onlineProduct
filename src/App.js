e4e7d1eda387a7801256dedbfc8815d971b933f6
Pinkykumarii <pinky@quikieapps.com>
2022-04-04 09:01:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductDetail from "./containers/ProductDetail";
+import ProductList from "./containers/ProductList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<ProductList />} />
+          <Route exact path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 not Found!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
